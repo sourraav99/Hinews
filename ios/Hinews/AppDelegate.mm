820faa3040c5101f35a18ac4b5cadcdf4a2d687b
Sourabh singh <149767822+sourraav99@users.noreply.github.com>
2024-04-17 14:58:30 +0530

    ios launchscreen updated

--- ios/Hinews/AppDelegate.mm
+++ ios/Hinews/AppDelegate.mm
@@ -12,7 +12,7 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-
+[NSThread sleepForTimeInterval:1.5];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
