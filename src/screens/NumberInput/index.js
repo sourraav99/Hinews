ea556ad954c7687e16c589a3254e04f34301b0b4
Sourabh singh <149767822+sourraav99@users.noreply.github.com>
2024-04-28 13:43:39 +0530

    made conditions in NumberInput

--- src/screens/NumberInput/index.js
+++ src/screens/NumberInput/index.js
@@ -1,18 +1,33 @@
-import {
-  View,
-  Text,
-  SafeAreaView,
-} from 'react-native';
+import {View, Text, SafeAreaView, Alert} from 'react-native';
 import PhoneInput from 'react-native-phone-number-input';
 import styles from './styles';
-import { moderateScale, scale, moderateVerticalScale, verticalScale } from 'react-native-size-matters';
+import {
+  moderateScale,
+  scale,
+  moderateVerticalScale,
+  verticalScale,
+} from 'react-native-size-matters';
 import CustomeButton from '../../Components/CustomeButton';
+import {useState} from 'react';
+import {useNavigation} from '@react-navigation/native';
+import OtpScreen from '../OtpScreen';
+import colour from '../../styles/colour';
 
-const NumberScreen = ({ navigation }) => {
+const NumberScreen = ({navigation}) => {
+  // const navigation = useNavigation();
+  const [input, setInput] = useState('');
+
+  const handle = () => {
+    if (input.length === 10) {
+      navigation.navigate('Otp');
+    } else {
+      Alert.alert('Plz Enter valid mobile number');
+    }
+  };
+  const isButtonDisabled = input.length !== 10;
   return (
     <SafeAreaView style={styles.SafeAreaMain}>
       <View style={styles.Viewmain}>
-
         <View style={styles.titleAndSubtitleContainer}>
           <Text style={styles.title}>My mobile</Text>
           <Text style={styles.subtitle}>
@@ -20,15 +35,25 @@ const NumberScreen = ({ navigation }) => {
             to verify your account.
           </Text>
         </View>
-        <View
-          style={styles.phoneInputViewStyle}>
-          <PhoneInput />
+        <View style={styles.phoneInputViewStyle}>
+          <PhoneInput
+            value={input}
+            onChangeText={val => {
+              setInput(val);
+            }}
+          />
         </View>
         <CustomeButton
-        btnstyle={styles.ContinueBtn}
-        btnText={'Continue'} 
-        btnTextStyle={{fontSize: scale(18),fontWeight: 'bold',}}
-        onpress={()=>navigation.navigate('Otp')}/>
+          btnstyle={{
+            ...styles.ContinueBtn,
+            backgroundColor:
+              input.length === 10 ? colour.theme_Colour_red : 'grey',
+          }}
+          disabled={isButtonDisabled}
+          btnText={'Continue'}
+          btnTextStyle={{fontSize: scale(18), fontWeight: 'bold'}}
+          onpress={handle}
+        />
       </View>
     </SafeAreaView>
   );
